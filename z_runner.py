818332c4ad885060d2c129e778b1235734f38391
yohn-dezmon <38733157+yohn-dezmon@users.noreply.github.com>
2018-12-21 13:51:59 -0600

    Add files via upload

--- z_runner.py
+++ z_runner.py
@@ -5,23 +5,30 @@ from Scene import Scene_
 from Edge import Edge_
 from Thorn import Thorn_
 from Aban import Abandoned
-from Gigan import Gigantic_tree
+from Gigan import GiganticTree
 from Hole import Hole_
-from Pile import Pile_O_Dung
+from Pile import PileDung
 from River import River_
 from charac_fg import Character_
 
 
 class Runner(object):
-
+    """
+    This is the class that runs the game, and allows the user to enter into
+    different scenes. The program is written such that once a player enters
+    a scene, they can complete all of the activities within the methods available
+    in the class and with the methods inherited from the parent Scene_ class.
+    """
+    # This is a dictionary which consists of string key values that refer to
+    # the scene modules in the form of class instantiations.
     scenes = {
             'edge' : Edge_(),
             'thorn' : Thorn_(),
-            'pile' : Pile_O_Dung(),
+            'pile' : PileDung(),
             'aban_fire' : Abandoned(),
             'hole' : Hole_(),
             'river' : River_(),
-            'gigan_tree' : Gigantic_tree(),
+            'gigan_tree' : GiganticTree(),
             'nothing' : "hmmm nothing really here"
     }
 
@@ -29,10 +36,17 @@ class Runner(object):
         pass
 
     def start_edge(self):
+        # The runner class is instantiated and then calls this method to start_edge
+        # the game.
+
+        # Here the edge key is used to instantiate the Edge_ class/scene.
         edge = Runner.scenes.get('edge')
+        # Here the enter method is called for the Edge_ scene.
         edge.enter()
 
         while Character_.location == "Edge of Forest":
+            # This while loop takes the value of the scenes directional attributes
+            # and calls functions that allow entry into other scenes. 
             edge = Runner.scenes.get('edge')
             if edge.right == 'thorn':
                 self.into_thorn()
